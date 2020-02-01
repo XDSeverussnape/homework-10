d9d814b339ab6c03e97ca2f05590038ac35116d7
Roman <dubchakp@gmail.com>
2020-02-01 16:49:41 +0100

    rework 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import '../src/styles.css';
 import json from '../src/data.json';
 
 //var source = document.getElementById('template').innerHTML;
@@ -13,3 +13,25 @@ const source = document.getElementById('template').innerHTML;
 const template = Handlebars.compile(source);
 const html = template(json);
 document.querySelector('.js-menu').innerHTML = html;
+const checkbox = document.querySelector('.checkbox');
+const body = document.getElementsByTagName('body')[0];
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+console.log(Theme.LIGHT);
+console.log(Theme.DARK);
+
+checkbox.addEventListener('change', e => {
+  localStorage.setItem('color', 'white');
+  const color = localStorage.getItem('color');
+  console.log(color);
+
+  if (e.target.checked == true) {
+    localStorage.setItem('color', '#313131');
+    body.style.background = localStorage.getItem('color');
+    localStorage.setItem('check', 'true');
+    e.target.checked = localStorage.getItem('check');
+  }
+  console.log(e.target.checked);
+});
